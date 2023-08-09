c6fc60de010adb362d1382800bd68f0a980c6c82
Andrew <paintstone@gmail.com>
2023-08-08 18:58:13 -0700

    added VITALS.sql

---
+++ data/VITALS.sql
@@ -0,0 +1,12 @@
+CREATE TABLE VITALS(
+	Pid	VARCHAR(30)	NOT NULL,
+	Visit_id VARCHAR(30) NOT NULL,
+	Height_in INT,
+	Weight_lb FLOAT,
+	Temp_f FLOAT,
+	Pulse INT,
+	Resp_rate INT,
+	Bp_syst	INT,
+	Bp_diast INT,
+	SpO2 INT
+);
