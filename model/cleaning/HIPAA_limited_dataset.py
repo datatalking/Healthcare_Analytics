5d097bb03e9eae5f6d4d529742d0e2468c4f0f1d
Andrew <paintstone@gmail.com>
2023-10-06 16:37:24 -0700

    added CARDIAC.sql, added HIPAA_limited_dataset.py

--- model/cleaning/HIPAA_limited_dataset.py
+++ model/cleaning/HIPAA_limited_dataset.py
@@ -2,27 +2,27 @@ import re
 
 # TODO build extension to access index of past data imported
 regex_patterns = {
-	"names": r"[A-Za-z'-]+(?:\s[A-Za-z'-]+)*",
-	"street addresses": r"\d{1,5}\s[A-Za-z]+\s[A-Za-z]+\s[A-Za-z]+",
-	"telephone numbers": r"\d{3}-\d{3}-\d{4}",
-	"fax numbers": r"\d{3}-\d{3}-\d{4}",
-	"e-mail addresses": r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}",
-	"Social Security numbers": r"\d{3}-\d{2}-\d{4}",
-	"medical records numbers": r"\d{8}",
-	"health plan beneficiary numbers": r"\d{8}",
-	"account numbers": r"\d{4}-\d{4}-\d{4}-\d{4}",
-	"certificate license numbers": r"[A-Za-z0-9]{8,12}",
-	"vehicle identifiers and serial numbers": r"[A-Z0-9]{17}",
-	"device identifiers and serial numbers": r"[A-Za-z0-9]+",
-	"URLs": r"https?://[A-Za-z0-9.-]+/[A-Za-z0-9.-]+",
-	"IP address numbers": r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",
-	"biometric identifiers": r"[A-Za-z0-9]+",
-	"full face photos": r".*\.(jpg|jpeg|png|gif)",
+    "names": r"[A-Za-z'-]+(?:\s[A-Za-z'-]+)*",
+    "street addresses": r"\d{1,5}\s[A-Za-z]+\s[A-Za-z]+\s[A-Za-z]+",
+    "telephone numbers": r"\d{3}-\d{3}-\d{4}",
+    "fax numbers": r"\d{3}-\d{3}-\d{4}",
+    "e-mail addresses": r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}",
+    "Social Security numbers": r"\d{3}-\d{2}-\d{4}",
+    "medical records numbers": r"\d{8}",
+    "health plan beneficiary numbers": r"\d{8}",
+    "account numbers": r"\d{4}-\d{4}-\d{4}-\d{4}",
+    "certificate license numbers": r"[A-Za-z0-9]{8,12}",
+    "vehicle identifiers and serial numbers": r"[A-Z0-9]{17}",
+    "device identifiers and serial numbers": r"[A-Za-z0-9]+",
+    "URLs": r"https?://[A-Za-z0-9.-]+/[A-Za-z0-9.-]+",
+    "IP address numbers": r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}",
+    "biometric identifiers": r"[A-Za-z0-9]+",
+    "full face photos": r".*\.(jpg|jpeg|png|gif)",
 }
 
 # TODO look for other patterns than regex, NLP, NLTK, BERT
 compiled_patterns = {
-	key: re.compile(pattern) for key, pattern in regex_patterns.items()
+    key: re.compile(pattern) for key, pattern in regex_patterns.items()
 }
 
 # TODO create index of text containing the specified data
@@ -47,10 +47,10 @@ Photo: sample.jpg
 # TODO add elastic search for matches in the sample text
 matches = {}
 for key, pattern in compiled_patterns.items():
-	match = pattern.search(sample_text)
-	if match:
-		matches[key] = match.group()
+    match = pattern.search(sample_text)
+    if match:
+        matches[key] = match.group()
 
 # TODO expand to display more than just the matched data
 for key, value in matches.items():
-	print(f"{key}: {value}")
+    print(f"{key}: {value}")
